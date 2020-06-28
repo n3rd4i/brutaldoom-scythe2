2f6427cda70ab6395a1f0edb4e00fdb6fd38b78f
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-28 16:10:55 +0200

    major refactoring

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,41 +3,19 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$toolsDir\commonEnv.ps1"
 . "$toolsDir\dependenciesEnv.ps1"
 
-$url = 'ftp://ftp.fu-berlin.de/pc/games/idgames/levels/doom2/Ports/megawads/scythe2.zip'
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $installLocation
-  url           = $url
-  softwareName  = 'brutaldoom-scythe2*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = 'ED108FC02D0615C4CAC9B70BA6D8E205'
-  checksumType  = 'md5'
-}
-Install-ChocolateyZipPackage @packageArgs
-
-$WAD = "`"$installLocation\scythe2.WAD`""
-
-## StartMenu
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\$ModName [BrutalDoom].lnk" `
-  -TargetPath "$zandronum" -Arguments "$BDpk3 -file $WAD -iwad $iWAD2" `
-  -WorkingDirectory "$installLocation"
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\$ModName [FreeDoom].lnk" `
-  -TargetPath "$zandronum" -Arguments "-file $WAD -iwad $iWAD2" `
-  -WorkingDirectory "$installLocation" `
-  -IconLocation "$FDicon"
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\$ModName Release Notes.lnk" `
-  -TargetPath "$installLocation\scythe2.txt"
-
 ## StartMenu - Multiplayer
 $SMMultiplayerDir = "$startMenuDir\Multiplayer"
-Install-ChocolateyShortcut -ShortcutFilePath "$SMMultiplayerDir\$ModName [MP] startServer [LAN].lnk" `
-  -TargetPath "$zandronum" -Arguments "$BDpk3 -file $WAD -iwad $iWAD2 -host -port 10666" `
+Install-ChocolateyShortcut "$SMMultiplayerDir\$ModName [MP] startServer [LAN].lnk" `
+  -TargetPath "$zandronum" -Arguments "$BDpk3 $WAD -iwad $iWAD2 -host -port 10666" `
   -WorkingDirectory "$installLocation"
-Install-ChocolateyShortcut -ShortcutFilePath "$SMMultiplayerDir\$ModName [MP] joinServer [LAN].lnk" `
-  -TargetPath "$zandronum" -Arguments "$BDpk3 -file $WAD -iwad $iWAD2 -connect 127.0.0.1:10666" `
+Install-ChocolateyShortcut "$SMMultiplayerDir\$ModName [MP] joinServer [LAN].lnk" `
+  -TargetPath "$zandronum" -Arguments "$BDpk3 $WAD -iwad $iWAD2 -connect 127.0.0.1:10666" `
   -WorkingDirectory "$installLocation"
 
-## Desktop
-Install-ChocolateyShortcut -ShortcutFilePath "$lnkDesktop" `
-  -TargetPath "$zandronum" -Arguments "$BDpk3 -file $WAD -iwad $iWAD2" `
-  -WorkingDirectory "$installLocation" `
-  -IconLocation "$BDicon"
+Install-ChocolateyShortcut "$startMenuDir\$ModName Release Notes.lnk" `
+  -TargetPath "$installLocation\scythe2.txt"
+
+## StartMenu
+Install-ChocolateyShortcut "$startMenuDir\$ModName [BrutalDoom].lnk" `
+  -TargetPath "$zandronum" -Arguments "$BDpk3 $WAD -iwad $iWAD2" `
+  -WorkingDirectory "$installLocation"
--- tools/dependenciesEnv.ps1
+++ tools/dependenciesEnv.ps1
@@ -1,5 +1,4 @@
 $zandronumLocation = "$ENV:LocalAppData\Programs\Zandronum"
-# $freedoomLocation = "$ENV:LocalAppData\Programs\FreeDoom"
 $brutalDoomLocation = "$ENV:LocalAppData\Programs\BrutalDoom"
 
 $freeDoomTools = "$ENV:ChocolateyInstall\lib\freedoom\tools"
@@ -7,12 +6,9 @@ $brutalDoomTools = "$ENV:ChocolateyInstall\lib\brutaldoom\tools"
 
 $zandronum = "$zandronumLocation\zandronum.exe"
 
-# $iWAD1 = "freedoom1.wad"
-$iWAD2 = "freedoom2.wad" # located in $ENV:DOOMWADDIR
+$iWAD2 = 'freedoom2.wad'
+$WAD = 'scythe2.wad'
 
-$BDModName = 'brutalv21'
-$BDModPack = "$BDModName.pk3"
-$BDpk3 = "$brutalDoomLocation\$BDModPack"
-
-$FDicon = "$freeDoomTools\assets\playa2a8.ico"
 $BDicon = "$brutalDoomTools\assets\brutal_doom.ico"
+$BDModPack = "brutalv21.pk3"
+$BDpk3 = "$brutalDoomLocation\$BDModPack"
